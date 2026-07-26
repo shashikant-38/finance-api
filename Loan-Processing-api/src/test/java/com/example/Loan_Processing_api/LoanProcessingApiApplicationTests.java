2194d0c08699b711a72886f4d30338dcef85a942
shashikant-38 <patilshashikant4508@gmail.com>
2026-07-27 00:36:46 +0530

    initial commit

---
+++ Loan-Processing-api/src/test/java/com/example/Loan_Processing_api/LoanProcessingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Loan_Processing_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanProcessingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
